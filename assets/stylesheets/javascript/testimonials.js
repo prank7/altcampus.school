c06604f84d2570cd35f36611ac32245a8d302cc3
abhigoswami12 <abhishekgoswami157@gmail.com>
2020-07-06 14:03:27 +0530

    work in progress

--- assets/stylesheets/javascript/testimonials.js
+++ assets/stylesheets/javascript/testimonials.js
@@ -7,25 +7,27 @@
     // elm.style.transform = "translateX(0)";
     // elm.style.transition = "all 2s ease";
     elm.classList.add("active");
+    // elm.classList.add("inactive");
+
 
 function handleNavRight(e) {
 
     let elm = document.getElementById(`testimonial${activeIndex}`);
-    // elm.style.transform = "translateX(-100%)";
-    // elm.style.transition =  "all 2s ease";
     elm.classList.remove("active");
+    // elm.classList.remove("inactive");
     activeIndex += 1;
     activeIndex != 4 ? "" : activeIndex = 0;
     let nextElm = document.getElementById(`testimonial${activeIndex}`);
-    // nextElm.style.transform = "translateX(0)";
-    // nextElm.style.transition = "all 2s ease";
     nextElm.classList.add("active");
+    // nextElm.classList.add("inactive");
 }
 function handleNavLeft(e) {
     document.getElementById(`testimonial${activeIndex}`).classList.remove("active");
+    // document.getElementById(`testimonial${activeIndex}`).classList.remove("inactive");
     activeIndex -= 1;
     activeIndex == -1 ? activeIndex = 3 : "";
     document.getElementById(`testimonial${activeIndex}`).classList.add("active");
+    // document.getElementById(`testimonial${activeIndex}`).classList.add("inactive");
 }
 navRight.addEventListener('click', handleNavRight);
 navLeft.addEventListener('click', handleNavLeft);
