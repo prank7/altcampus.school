f91f76fc1f76adc8a7d3667228e14dffef4e8fa5
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2020-09-09 18:10:59 +0530

    add community og

--- components/Layout.js
+++ components/Layout.js
@@ -15,10 +15,7 @@ export default function Layout({ children }) {
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+          content="/images/AltCampus-Community-OG.png" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
