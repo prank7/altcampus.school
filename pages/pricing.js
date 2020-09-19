0a31446c4aa5409f562ab493fb48d5626bdd45cd
Ankit <iankit17@gmail.com>
2020-09-19 23:36:46 +0530

    convert other pages to components

---
+++ pages/pricing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Pricing from '../components/Home/Pricing';
+import Footer from '../components/Home/Footer';
+import Header from '../components/Home/Header';
+
+function PricingPage(props) {
+  return (
+    <>
+      <Header />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
+
+export default PricingPage;
