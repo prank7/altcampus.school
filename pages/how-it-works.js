55929d8f395a0ea178d7a506e4249ae5127f9c4c
suraj_122 <122suraj0@gmail.com>
2020-09-23 03:14:48 +0530

    how it works page convert to tailwind

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import LayoutHome from '../components/Home/Layout';
+import LayoutHome from "../components/Home/Layout";
 
 function HowItWorks(props) {
   return (
@@ -12,11 +12,13 @@ function HowItWorks(props) {
 
 function Content(props) {
   return (
-    <main className='pricing-policycontainer pricing-policy padding-top-0 how-it-works '>
-      <h1>How it works</h1>
-      <h5>Student Journey</h5>
-      <h6>Onboarding</h6>
-      <p>
+    <main className="max-w-3xl mx-auto px-8 py-16">
+      <h1 className="text-center text-dark-blue-500 text-5xl font-semibold mb-8">
+        How it works
+      </h1>
+      <h5 className="text-3xl text-green-theme-500">Student Journey</h5>
+      <h6 className="text-2xl text-royal-blue-500 mt-8 mb-3">Onboarding</h6>
+      <p className="text-dark-blue-300 text-lg">
         As a student, once you signup you will be onboarded on our learning
         platform. Onboarding will involve connecting other platforms and tools
         and watching an orientation video on how to approach the course. You
@@ -29,8 +31,10 @@ function Content(props) {
         there.
       </p>
 
-      <h6>Content, Assignments, Projects & Feedback</h6>
-      <p>
+      <h6 className="text-2xl text-royal-blue-500 mt-8 mb-3">
+        Content, Assignments, Projects & Feedback
+      </h6>
+      <p className="text-dark-blue-300 text-lg">
         Each module has concepts delivered using videos and it's followed up by
         quizzes and extensive exercises, small to big. We have made sure that
         there is enough practice material for you to master each concept.
@@ -41,7 +45,7 @@ function Content(props) {
         development. At each step, mentors will provide you with extra resources
         and tips to help you learn better.
       </p>
-      <p>
+      <p className="text-dark-blue-300 text-lg">
         Once you have completed an exercise or project, there are two levels of
         feedback provided. One is automated feedback, which is a pre-recorded
         video of a mentor explaining the approach of solving the problem. This
@@ -54,8 +58,10 @@ function Content(props) {
         forward.
       </p>
 
-      <h6>Mentor Office hours and Getting help</h6>
-      <p>
+      <h6 className="text-2xl text-royal-blue-500 mt-8 mb-3">
+        Mentor Office hours and Getting help
+      </h6>
+      <p className="text-dark-blue-300 text-lg">
         Every working day, the corresponding mentor will hold an office hour
         through a live video call. Mentor will provide feedback and help you
         resolve your doubts in the session. That's one way of getting help,
@@ -69,24 +75,28 @@ function Content(props) {
         and keep you focused on the learning path. We are here to help.
       </p>
 
-      <h6>Tracking progress and accountability</h6>
-      <p>
+      <h6 className="text-2xl text-royal-blue-500 mt-8 mb-3">
+        Tracking progress and accountability
+      </h6>
+      <p className="text-dark-blue-300 text-lg">
         We have laid a strong focus on consistency. The platform measures and
         encourages you to be consistent. Your mentors will closely follow your
         progress and if need be follow up with you and set up a separate
         one-to-one call to motivate, counsel, and help you move forward. We
         deeply care that anyone who starts the course actually finishes it.
       </p>
-      <p>
-        Everyone is encouraged to tweet their progress,{' '}
-        <a target='_blank' href='https://twitter.com/search?q=altcampus'>
+      <p className="text-dark-blue-300 text-lg">
+        Everyone is encouraged to tweet their progress,{" "}
+        <a target="_blank" href="https://twitter.com/search?q=altcampus">
           examples of which you can see here
         </a>
         .
       </p>
 
-      <h6>Peer group, Collaboration and Community</h6>
-      <p>
+      <h6 className="text-2xl text-royal-blue-500 mt-8 mb-3">
+        Peer group, Collaboration and Community
+      </h6>
+      <p className="text-dark-blue-300 text-lg">
         The learning platform will match you with people moving at your pace.
         You can personally connect to them via Slack. In general, you can share
         and help each other out through common module based topics. We conduct
@@ -97,8 +107,8 @@ function Content(props) {
         AltCampus alumni and offline student community.
       </p>
 
-      <h6>Job support</h6>
-      <p className='margin-bottom-4'>
+      <h6 className="text-2xl text-royal-blue-500 mt-8 mb-3">Job support</h6>
+      <p className="text-dark-blue-300 text-lg">
         Throughout the course, we make sure that whatever you do is geared
         towards making a solid portfolio. Once you have finished all the
         modules, you will be given a rigorous 'Job Readiness Challenge', which
@@ -109,10 +119,10 @@ function Content(props) {
         interview, you can either take it up or continue to look for other jobs.
       </p>
 
-      <div className='flex-center margin-bottom-4'>
+      <div className="text-center mt-12">
         <a
-          className='btn btn-success'
-          href='https://launchpad.altcampus.school/signup'
+          className="bg-green-theme-500 text-white text-lg px-12 font-medium py-4 rounded"
+          href="https://launchpad.altcampus.school/signup"
         >
           START LEARNING NOW
         </a>
