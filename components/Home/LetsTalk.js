5ed57e37525a12b1245472a8a1a3658ff9f9b103
suraj_122 <122suraj0@gmail.com>
2020-09-22 18:57:41 +0530

    update homepage with tailwid

--- components/Home/LetsTalk.js
+++ components/Home/LetsTalk.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function LetsTalk(props) {
   return (
-    <section className='padding cta'>
-      <div className='container'>
-        <div className='cta-wrapper'>
+    <section className="cta py-24">
+      <div className="container mx-auto">
+        <div className="px-12 text-center sm:text-left sm:flex sm:items-center sm:justify-center">
           <div>
-            <h3>Still have doubts? Talk to us!</h3>
-            <h5>Get on a call with our co-founder.</h5>
+            <h3 className="text-3xl text-white max-w-lg mr-8">
+              Still have doubts? Talk to us!
+            </h3>
+            <h5 className="text-white">Get on a call with our co-founder.</h5>
           </div>
 
           <a
-            className='btn'
-            href='https://calendly.com/prank7/altcampus-one-on-one'
-            target='_blank'
+            className="mt-4 sm:mt-0 bg-white uppercase text-dark-blue-500 py-4 px-12 inline-block rounded shadow-md font-semibold"
+            href="https://launchpad.altcampus.school/signup"
           >
             Let's talk!
           </a>
--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,19 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Navbar from './Navbar';
+import Head from "next/head";
+import Link from "next/link";
+import Navbar from "./Navbar";
 
-const siteTitle = "AltCampus Community"
+const siteTitle = "AltCampus Community";
 
 export default function Layout({ children }) {
   return (
     <div>
       <Head>
         <link rel="icon" href="/favicon.png" />
-        <meta
-          name="description"
-          content="AltCampus Community"
-        />
+        <meta name="description" content="AltCampus Community" />
         <meta
           property="og:image"
-          content="/images/AltCampus-Community-OG.png" />
+          content="/images/AltCampus-Community-OG.png"
+        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
@@ -24,5 +22,5 @@ export default function Layout({ children }) {
         {children}
       </main>
     </div>
-  )
+  );
 }
