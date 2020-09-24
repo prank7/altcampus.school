03472a9cfb443cb25c73feced8b78662ca903d7e
Ankit <iankit17@gmail.com>
2020-09-24 14:50:44 +0530

    add link to footer

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <header className='py-4 border-b-2 border-green-theme-50 z-20 relative'>
-      <nav className='container flex justify-between mx-auto items-center px-8 relative z-10'>
+    <header className='border-b-2 z-20 relative py-3'>
+      <nav className='container flex justify-between mx-auto items-center px-6 relative z-10 max-w-6xl'>
         <Link href='/'>
           <a>
             <img className='w-32' src='/assets/media/logo.svg' alt='Logo' />
@@ -33,35 +33,35 @@ function Header() {
           <ul className='md:flex md:items-center'>
             <li className='mb-8 md:mb-0'>
               <ActiveLink href='/'>
-                <a className='text-4xl md:text-xl text-gold-500 md:text-dark-blue-300 py-1'>
+                <a className='text-4xl font- md:text-xl text-gold-500 md:text-dark-blue-300 pt-1 hover:text-dark-blue-500'>
                   Home
                 </a>
               </ActiveLink>
             </li>
-            <li className='mb-8 md:mb-0 md:ml-8'>
+            <li className='mb-8 md:mb-0 md:ml-10'>
               <ActiveLink href='/how-it-works'>
-                <a className='text-4xl md:text-xl text-gold-500  md:text-dark-blue-300 py-1'>
+                <a className='text-4xl md:text-xl text-gold-500  md:text-dark-blue-300 pt-1 hover:text-dark-blue-500'>
                   How it works
                 </a>
               </ActiveLink>
             </li>
-            <li className='mb-8 md:mb-0 md:ml-8'>
+            <li className='mb-8 md:mb-0 md:ml-10'>
               <ActiveLink href='/pricing'>
-                <a className='text-4xl md:text-xl text-gold-500  md:text-dark-blue-300 py-1'>
+                <a className='text-4xl md:text-xl text-gold-500  md:text-dark-blue-300 pt-1 hover:text-dark-blue-500'>
                   Pricing
                 </a>
               </ActiveLink>
             </li>
-            <li className='mb-8 md:mb-0 md:ml-8'>
+            <li className='mb-8 md:mb-0 md:ml-10'>
               <ActiveLink href='#faqs'>
-                <a className='text-4xl md:text-xl text-gold-500  md:text-dark-blue-300 py-1'>
+                <a className='text-4xl md:text-xl text-gold-500  md:text-dark-blue-300 pt-1 hover:text-dark-blue-500'>
                   FAQs
                 </a>
               </ActiveLink>
             </li>
-            <li className='mb-8 md:mb-0 md:ml-8'>
+            <li className='mb-8 md:mb-0 md:ml-10'>
               <a
-                className='inline-block text-xl md:text-base text-white md:text-dark-blue-300 uppercase px-12 py-4 md:px-4 md:py-2 border rounded border-green-theme-500 shadow-md font-medium bg-green-theme-500 md:bg-transparent'
+                className='inline-flex content-center items-center text-xl md:text-base text-white md:text-dark-blue-300 uppercase px-12 py-4 md:px-4 md:py-1 border rounded border-green-theme-500 shadow-md  bg-green-theme-500 md:bg-transparent hover:shadow-inner tracking-wide'
                 href='https://launchpad.altcampus.school/login'
               >
                 login
@@ -69,7 +69,7 @@ function Header() {
             </li>
             <li className='md:ml-4'>
               <a
-                className='font-normal inline-block text-xl md:text-base text-white px-12 py-4 md:px-4 md:py-2 rounded uppercase bg-green-theme-500 border border-green-theme-500 shadow-md'
+                className='inline-flex content-center items-center text-xl md:text-base text-white px-12 py-4 md:px-4 md:py-1 rounded uppercase bg-green-theme-500 border border-green-theme-500 shadow-md hover:shadow-inner tracking-wide'
                 href='https://launchpad.altcampus.school/signup'
               >
                 signup
