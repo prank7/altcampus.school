90d3d4bf2c3182e3ebffd5c9ebb02edd1b4dd96e
Ankit <iankit17@gmail.com>
2020-09-24 14:50:44 +0530

    add link to footer

--- components/Home/Footer.js
+++ components/Home/Footer.js
@@ -1,141 +1,132 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
 
 function Footer(props) {
   return (
-    <footer class="py-24 bg-dark-blue-500">
-      <div class="container mx-auto px-8 md:flex md:justify-between">
-        <div class="footer-col1-2 md:w-2/5">
-          <a class="footer-logo" href="/">
-            <img
-              className="w-28 inline-block"
-              src="/assets/media/logo.svg"
-              alt="AltCampus Logo"
-            />
-          </a>
-          <p class="text-sm text-gray-500 mt-4">
+    <footer class='py-24 bg-dark-blue-500'>
+      <div class='container mx-auto px-8 md:flex md:justify-between'>
+        <div class='footer-col1-2 md:w-2/5'>
+          <Link href='/'>
+            <a class='footer-logo'>
+              <img
+                className='w-28 inline-block'
+                src='/assets/media/logo.svg'
+                alt='AltCampus Logo'
+              />
+            </a>
+          </Link>
+          <p class='text-sm text-gray-500 mt-4'>
             Everything you need to become a software developer
           </p>
         </div>
-        <div class="footer-col1-2 footer-links md:w-3/5">
-          <div class="md:flex md:justify-between">
-            <div class="md:w-1/2">
-              <h4 className="text-2xl text-white mb-3 mt-6 md:mt-0">
+        <div class='footer-col1-2 footer-links md:w-3/5'>
+          <div class='md:flex md:justify-between'>
+            <div class='md:w-1/2'>
+              <h4 className='text-2xl text-white mb-3 mt-6 md:mt-0'>
                 Quick links
               </h4>
-              <div class="flex justify-between">
-                <ul class="">
-                  <li className="mb-2">
-                    <a className="text-gray-500 hover:text-gold-600" href="/">
-                      Home
-                    </a>
+              <div class='flex justify-between'>
+                <ul class=''>
+                  <li className='mb-2'>
+                    <Link href='/'>
+                      <a className='text-gray-500 hover:text-gold-600'>Home</a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/#course-details"
-                    >
-                      Course Details
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/#course-details'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        Course Details
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/how-it-works"
-                    >
-                      How it works
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/how-it-works'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        How it works
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/#testimonials"
-                    >
-                      Testimonials
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/#testimonials'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        Testimonials
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/pricing"
-                    >
-                      Pricing
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/pricing'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        Pricing
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/#faqs"
-                    >
-                      FAQs
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/#faqs'>
+                      <a className='text-gray-500 hover:text-gold-600'>FAQs</a>
+                    </Link>
                   </li>
                 </ul>
-                <ul class="quick-links">
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/contact"
-                      target="_blank"
-                    >
-                      Contact
-                    </a>
+                <ul class='quick-links'>
+                  <li className='mb-2'>
+                    <Link href='/contact'>
+                      <a
+                        className='text-gray-500 hover:text-gold-600'
+                        target='_blank'
+                      >
+                        Contact
+                      </a>
+                    </Link>
                   </li>
 
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/privacy"
-                    >
-                      Privacy
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/privacy'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        Privacy
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/refund-policy"
-                    >
-                      Refund Policy
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/refund-policy'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        Refund Policy
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/pricing-policy/"
-                    >
-                      Pricing Policy
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/pricing-policy/'>
+                      <a className='text-gray-500 hover:text-gold-600'>
+                        Pricing Policy
+                      </a>
+                    </Link>
                   </li>
-                  <li className="mb-2">
-                    <a
-                      className="text-gray-500 hover:text-gold-600"
-                      href="/terms/"
-                    >
-                      Terms
-                    </a>
+                  <li className='mb-2'>
+                    <Link href='/terms'>
+                      <a className='text-gray-500 hover:text-gold-600'>Terms</a>
+                    </Link>
                   </li>
                 </ul>
               </div>
             </div>
-            <div class="social">
-              <h4 className="text-2xl text-white mb-3 mt-6 md:mt-0">
+            <div class='social'>
+              <h4 className='text-2xl text-white mb-3 mt-6 md:mt-0'>
                 Follow Us
               </h4>
-              <ul class="flex">
-                <li className="mr-4">
-                  <a target="_blank" href="https://facebook.com/AltCampusIO">
+              <ul class='flex'>
+                <li className='mr-4'>
+                  <a target='_blank' href='https://facebook.com/AltCampusIO'>
                     <img
-                      className="w-12"
-                      src="/assets/media/facebook-icon.svg"
-                      alt="facebook-icon"
+                      className='w-12'
+                      src='/assets/media/facebook-icon.svg'
+                      alt='facebook-icon'
                     />
                   </a>
                 </li>
                 <li>
-                  <a target="_blank" href="https://twitter.com/altcampus">
+                  <a target='_blank' href='https://twitter.com/altcampus'>
                     <img
-                      className="w-12"
-                      src="/assets/media/twitter-icon.svg"
-                      alt="twitter-icon"
+                      className='w-12'
+                      src='/assets/media/twitter-icon.svg'
+                      alt='twitter-icon'
                     />
                   </a>
                 </li>
@@ -144,7 +135,7 @@ function Footer(props) {
           </div>
         </div>
       </div>
-      <div class="text-center mt-12 text-gray-700">
+      <div class='text-center mt-12 text-gray-700'>
         <small>&copy; AltCampus Services Pvt. Ltd, 2018-present.</small>
       </div>
     </footer>
--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,42 +1,42 @@
-import React from "react";
+import React from 'react';
 
 function Hero(props) {
   return (
-    <section className="hero text-center py-20 md:py-32 relative overflow-hidden">
-      <div className="container mx-auto px-8">
+    <section className='hero text-center py-20 md:pt-32 md:pb-24 relative overflow-hidden'>
+      <div className='container mx-auto px-8'>
         <div>
-          <h1 className="font-semibold text-dark-blue-500 text-4xl md:text-6xl px-2 leading-tight">
+          <h1 className='hero-heading font-semibold text-dark-blue-500 text-4xl px-2 leading-tight max-w-4xl mx-auto'>
             Everything you need to become a software developer
           </h1>
-          <h3 className="text-2xl text-dark-blue-300 font-light my-12">
+          <h3 className='text-2xl text-dark-blue-300 font-light my-12'>
             A program designed to help you develop full-stack web development
             skills and <br />
             become job-ready
           </h3>
-          <div className="flex justify-center">
-            <h5 className="text-royal-blue-400 font-semibold text-lg">
+          <div className='flex justify-center'>
+            <h5 className='text-royal-blue-400 font-semibold text-lg'>
               100% online
             </h5>
-            <span className="dot"></span>
-            <h5 className="text-royal-blue-400 mx-8 font-semibold text-lg">
+            <span className='dot'></span>
+            <h5 className='text-royal-blue-400 mx-8 font-semibold text-lg'>
               Project based
             </h5>
-            <span className="dot"></span>
-            <h5 className="text-royal-blue-400 font-semibold text-lg">
+            <span className='dot'></span>
+            <h5 className='text-royal-blue-400 font-semibold text-lg'>
               Mentorship
             </h5>
           </div>
           <a
-            href="https://launchpad.altcampus.school/signup"
-            className="text-lg text-white bg-green-theme-500 uppercase py-4 px-24 inline-block mt-12 rounded font-semibold hover:shadow-lg"
+            href='https://launchpad.altcampus.school/signup'
+            className='text-lg text-white bg-green-theme-500 uppercase py-4 px-24 inline-block mt-12 rounded font-semibold hover:shadow-lg'
           >
             Start Learning
           </a>
         </div>
         <img
-          className="absolute right-0 top-0 hero-image"
-          src="/assets/media/bg-hero1.svg"
-          alt="Hero Bg"
+          className='absolute right-0 top-0 hero-image'
+          src='/assets/media/bg-hero1.svg'
+          alt='Hero Bg'
         />
       </div>
     </section>
