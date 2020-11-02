71e73ba32bc20b709743ccd21ec941062da8582b
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2020-11-02 16:56:07 +0530

    update the tutorial side

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charset='UTF-8' />
+          <meta charSet='UTF-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <meta
             name='description'
