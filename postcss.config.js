8064effa8ed1a2278c2ca04e5686bf30b5869e11
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2021-03-02 13:15:10 +0530

    update index.css

--- postcss.config.js
+++ postcss.config.js
@@ -13,17 +13,6 @@ module.exports = {
           "custom-properties": false,
         },
       },
-    ],
-    [
-      '@fullhuman/postcss-purgecss',
-      process.env.NODE_ENV === 'production' ? {
-        content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}'
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body"]
-      } : false,
     ]
   ]
 };
