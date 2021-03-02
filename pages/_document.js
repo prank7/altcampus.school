346672f3ebe0112a5260bfe474f247c2474467fc
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2021-03-02 16:49:56 +0530

    change the title

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ class MyDocument extends Document {
             href='/assets/media/favicon.png'
           />
           
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin="true"/>
+          <link rel='preconnect' href='https://fonts.googleapis.com' crossOrigin="true"/>
           <link
             href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Source+Sans+Pro:wght@200;400;600;700&family=Mukta:wght@300;400;500;600&display=swap'
             rel="preload"
