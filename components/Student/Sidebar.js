e3a7a2e4d0b334d7241f3e0a5c6ef379d0453c56
suraj_122 <122suraj0@gmail.com>
2021-07-11 16:08:17 +0530

    update students information with background details

--- components/Student/Sidebar.js
+++ components/Student/Sidebar.js
@@ -6,17 +6,17 @@ function Sidebar(props) {
       <article>
         <figure className="">
           <img
-            className="rounded-lg"
+            className="rounded-lg max-h-9"
             src="/images/students/Harshaan.jpeg"
             alt="Harshaan Nihaal Khaan"
           />
         </figure>
-        <footer className="text-center p-3 font-bold text-dark-blue-600 capitalize text-2xl">
+        <div className="text-center p-3 font-bold text-dark-blue-600 capitalize text-2xl">
           <h2>Harshaan Nihaal Khan</h2>
           <ul className="flex items-center justify-center mt-2">
             <li className="mx-2">
               <a
-                className="w-10 h-10 bg-dark-blue-200 block place-items-center grid rounded-full"
+                className="w-10 h-10 bg-dark-blue-200 place-items-center grid rounded-full"
                 href="#"
               >
                 <img src="/images/icons/github.svg" alt="GitHub" />
@@ -24,7 +24,7 @@ function Sidebar(props) {
             </li>
             <li className="mx-2">
               <a
-                className="w-10 h-10 bg-dark-blue-200 block place-items-center grid rounded-full"
+                className="w-10 h-10 bg-dark-blue-200 place-items-center grid rounded-full"
                 href="#"
               >
                 <img src="/images/icons/twitter-sm.svg" alt="Twitter" />
@@ -32,7 +32,7 @@ function Sidebar(props) {
             </li>
             <li className="mx-2">
               <a
-                className="w-10 h-10 bg-dark-blue-200 block place-items-center grid rounded-full"
+                className="w-10 h-10 bg-dark-blue-200 place-items-center grid rounded-full"
                 href="#"
               >
                 <img src="/images/icons/linkedin.svg" alt="Linkedin" />
@@ -40,14 +40,80 @@ function Sidebar(props) {
             </li>
             <li className="mx-2">
               <a
-                className="w-10 h-10 bg-dark-blue-200 block place-items-center grid rounded-full"
+                className="w-10 h-10 bg-dark-blue-200 place-items-center grid rounded-full"
                 href="#"
               >
                 <img src="/images/icons/email.svg" alt="Email" />
               </a>
             </li>
           </ul>
-        </footer>
+        </div>
+        {/* <input type="checkbox" id="show-background" hidden />
+        <label
+          className="flex items-center justify-center text-lg pb-1"
+          for="show-background"
+        >
+          <span className="hover:bg-dark-blue-200 rounded-sm py-0 px-3 mt-2 cursor-pointer flex items-center">
+            See more
+            <img
+              className="w-4 ml-1 mt-1"
+              src="/images/icons/caret-down.svg"
+              alt="Caret Down"
+            />
+          </span>
+        </label> */}
+        <div className="px-8 py-2 student-background">
+          <h2 className="text-xl font-bold text-dark-blue-700 border-t-2 pt-2">
+            Background
+          </h2>
+          <div className="mt-5">
+            <h3 className="text-lg text-dark-blue-300 font-semibold mb-5">
+              Education
+            </h3>
+            <div className="flex items-start">
+              <img
+                src="/images/icons/brand-icon.svg"
+                className="w-8"
+                alt="Brand"
+              />
+              <div className="ml-6">
+                <h3 className="text-md font-semibold text-dark-blue-700">
+                  Full Stack Certification
+                </h3>
+                <h4 className="text-md font-semibold text-dark-blue-700">
+                  AltCampus School
+                </h4>
+                <time
+                  className="text-md font-semibold text-dark-blue-300"
+                  datetime=""
+                >
+                  2018-2019
+                </time>
+              </div>
+            </div>
+            <div className="flex items-start mt-4">
+              <img
+                src="/images/icons/grad-hat.svg"
+                className="w-8"
+                alt="Graduation Hat"
+              />
+              <div className="ml-6">
+                <h3 className="text-md font-semibold text-dark-blue-700">
+                  University of California
+                </h3>
+                <h4 className="text-md font-semibold text-dark-blue-700">
+                  B.com, Commerce
+                </h4>
+                <time
+                  className="text-md font-semibold text-dark-blue-300"
+                  datetime=""
+                >
+                  2014-2017
+                </time>
+              </div>
+            </div>
+          </div>
+        </div>
       </article>
     </aside>
   );
