0544f29ebef33e992c6c4242ae22b8a0455d2646
suraj_122 <122suraj0@gmail.com>
2021-07-12 11:13:17 +0530

    add placement card

---
+++ components/Placement/student.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Student(props) {
+  return (
+    <a href="/placements/student">
+      <article className="border rounded-lg shadow-sm">
+        <figure className="h-56 overflow-hidden">
+          <img
+            className="rounded-lg rounded-b-none h-full w-full object-cover"
+            src="/images/students/harshaan-full.jpeg"
+            alt="Harshaan Nihaal Khaan"
+          />
+        </figure>
+        <div className="px-4 py-2">
+          <h2 className="text-2xl text-dark-blue-700 font-bold">
+            Harshaan Nihaal Khan
+          </h2>
+          <h4 className="text-md text-gray-800 font-semibold mt-1">
+            Software Engineer, BigBinary
+          </h4>
+          <h4 className="text-right text-lg text-gray-500 font-semibold mt-3 font-sans">
+            CTA: $2000
+          </h4>
+        </div>
+      </article>
+    </a>
+  );
+}
+
+export default Student;
--- pages/placements/index.js
+++ pages/placements/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import LayoutHome from '../../components/Home/Layout';
 import Head from 'next/head';
+import Student from '../../components/Placement/student';
 
 function Placement() {
   return (
-    <LayoutHome>
+    <div>
       <Head>
         <title>Placements | AltCampus Community </title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-    </LayoutHome>
+      <LayoutHome>
+        <section className="bg-royal-blue-100 py-16">
+          <article className="container mx-auto text-center">
+            <h1 className="text-3xl font-bold">Placement Record</h1>
+          </article>
+        </section>
+        <section className="py-16">
+          <div className="container mx-auto grid grid-cols-4 gap-8">
+            <Student />
+            <Student />
+            <Student />
+            <Student />
+            <Student />
+          </div>
+        </section>
+      </LayoutHome>
+    </div>
   );
 }
 
