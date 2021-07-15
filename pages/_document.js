3219b71d758d2f4ffae7f7dcf8eca484db2d7564
Vipin <vipineth@gmail.com>
2021-07-15 16:21:03 +0530

    move head from _document to _app

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Main, NextScript } from 'next/document';
 
-const GA_TRACKING_ID = 'UA-121886584-4';
+
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,45 +11,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta charSet='UTF-8' />
-          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-          <meta name='robots' content='index, follow' />
-          <meta name="author" content="AltCampus" />
-          <meta name="HandheldFriendly" content="True" />
-          <meta name="MobileOptimized" content="320" />
-          <link
-            rel='shortcut icon'
-            type='image/png'
-            href='/assets/media/favicon.png'
-          />
-          
-          <link rel='preconnect' href='https://fonts.googleapis.com' crossOrigin="true"/>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Source+Sans+Pro:wght@200;400;600;700&family=Mukta:wght@300;400;500;600&display=swap'
-            rel="preload"
-            as="style"
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Source+Sans+Pro:wght@200;400;600;700&family=Mukta:wght@300;400;500;600&display=swap'
-            rel="stylesheet"
-          />
 
-          <script
-            async={true}
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_TRACKING_ID}');
-            `,
-            }}
-          />
-        </Head>
         <body className='antialiased'>
           <Main />
           <NextScript />
