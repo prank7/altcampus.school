cba6fe4fe70632c953813f2d850d39ddbb95c80e
Vipin <vipineth@gmail.com>
2021-07-15 17:41:12 +0530

    move head style to _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+import Document, { Html, Main,Head, NextScript } from 'next/document';
 
+const GA_TRACKING_ID = 'UA-121886584-4';
 
 
 class MyDocument extends Document {
@@ -11,7 +12,40 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+ <Head>
 
+          <link
+            rel='shortcut icon'
+            type='image/png'
+            href='/assets/media/favicon.png'
+          />
+
+          <link rel='preconnect' href='https://fonts.googleapis.com' crossOrigin="true"/>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Source+Sans+Pro:wght@200;400;600;700&family=Mukta:wght@300;400;500;600&display=swap'
+            rel="preload"
+            as="style"
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Source+Sans+Pro:wght@200;400;600;700&family=Mukta:wght@300;400;500;600&display=swap'
+            rel="stylesheet"
+          />
+
+          <script
+            async={true}
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+            `,
+            }}
+          />
+        </Head>
         <body className='antialiased'>
           <Main />
           <NextScript />
