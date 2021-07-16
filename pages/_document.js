1052764cad4902922fa73ce5079be72d88fa04a3
Ankit <iankit17@gmail.com>
2021-07-16 10:32:25 +0530

    change font as preload

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,8 @@ class MyDocument extends Document {
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700,900&family=Source+Sans+Pro:wght@200;400;600;700&family=Mukta:wght@300;400;500;600&display=swap"
-            rel="stylesheet"
+            as="style"
+            rel="preload"
           />
 
           <script
