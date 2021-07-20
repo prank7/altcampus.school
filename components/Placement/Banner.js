6d5c46b5343ddee03188e07994d3e1fd036e28b6
Sunny Singh <sunnysinghrajavatssr@gmail.com>
2021-07-20 21:28:20 +0530

    Update Thappa icon to png

--- components/Placement/Banner.js
+++ components/Placement/Banner.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 function Banner(props) {
@@ -14,11 +15,16 @@ function Banner(props) {
           tech.
         </h2>
         <div className="text-center">
-          <img
-            src="/images/icons/success-rate.svg"
-            alt="Success Rate Stamp"
-            className="w-24 mt-8 mb-3 inline-block"
-          />
+          <figure className="w-24 mt-8 mb-3 inline-block">
+            <Image
+              layout="responsive"
+              src="/images/icons/success-rate.png"
+              alt="Success Rate Stamp"
+              height={100}
+              width={100}
+              // className=""
+            />
+          </figure>
         </div>
         <h3 className="text-4xl text-royal-blue-500 font-bold">
           <strong>Our Placement Success Rate - 86.4%</strong>
