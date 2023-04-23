2ceebb539ea7b4e0bc77c6b1d63411b51aed6133
suraj_122 <122suraj0@gmail.com>
2023-04-23 16:16:46 +0530

    add track list for course

---
+++ components/Courses/TrackCard.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import React from 'react';
+
+function TrackCard({ title, icon }) {
+  return (
+    <article className="border border-gray-300 flex items-stretch justify-between mx-auto rounded-md">
+      <header className="py-4 pl-4">
+        <h2 className="text-2xl text-royal-blue-800 font-semibold">{title}</h2>
+        <p className="text-base text-gray-500">
+          Curabitur lobortis id lorem id bibendum. Ut id consectetur magna.
+        </p>
+        <div className="flex items-center mt-5 text-gray-500 gap-x-2">
+          <strong className="inline-block bg-gold-400 bg-opacity-40 p-2 rounded-md font-medium text-royal-blue-800">
+            Specific Module ★
+          </strong>
+          <span>•</span>
+          <strong className="text-gray-500 font-semibold">16-32 weeks</strong>
+        </div>
+      </header>
+
+      <figure className="">
+        <img
+          className="h-full inline-block"
+          src={`/images/icons/${icon}.svg`}
+          alt="HTML"
+        />
+      </figure>
+    </article>
+  );
+}
+
+export default TrackCard;
---
+++ components/Courses/TrackList.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+import TrackCard from './TrackCard';
+
+function TrackList() {
+  return (
+    <section className="py-12 ">
+      <div className="max-w-6xl mx-auto px-8">
+        <h4 className="text-xl text-gray-500 font-semibold text-center mb-8">
+          Skills that you will learn in this course
+        </h4>
+        <div className="grid md:grid-cols-2 gap-4">
+          <TrackCard title="HTML & CSS" icon="html-large" />
+          <TrackCard title="Javascript" icon="js-large" />
+          <TrackCard title="React Js" icon="react-large" />
+          <TrackCard title="EL 6" icon="mongo-large" />
+          <TrackCard title="Mongo DB" icon="mongo-large" />
+          <TrackCard title="Node Js" icon="node-large" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TrackList;
