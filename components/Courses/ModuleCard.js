9327c94d31ef80b54ebee5aabbb2ac2b71f7c0eb
suraj_122 <122suraj0@gmail.com>
2023-04-23 21:23:09 +0530

    style module card

--- components/Courses/ModuleCard.js
+++ components/Courses/ModuleCard.js
@@ -3,35 +3,39 @@ import React from 'react';
 
 function ModuleCard({ module }) {
   return (
-    <article className="border border-gray-300 flex items-stretch justify-between mx-auto rounded-md">
-      <header className="py-4 pl-4">
-        <h2 className="text-2xl text-royal-blue-800 font-semibold">{module.name}</h2>
-        <p className="text-base text-gray-500">
-          Curabitur lobortis id lorem id bibendum. Ut id consectetur magna.
-        </p>
-        <div className="flex items-center mt-5 text-gray-500 gap-x-2">
-          <strong className="inline-block bg-gold-400 bg-opacity-40 p-2 rounded-md font-medium text-royal-blue-800">
-            Specific Module ★
-          </strong>
-          <span>•</span>
-          <strong className="text-gray-500 font-semibold">16-32 weeks</strong>
-        </div>
+    <article className="border border-gray-300 mx-auto rounded-md p-4">
+      <header className="flex">
         <div>
-          <ul>
-            {module.topics.map((t, i) => {
-              return (<li key={i}>{t.name}</li>)
-            })}
-          </ul>
+          <h2 className="text-2xl text-royal-blue-800 font-semibold">
+            {module.name}
+          </h2>
+          <p className="text-base text-gray-500 max-w-sm mt-2">
+            Curabitur lobortis id lorem id bibendum. Ut id consectetur magna.
+          </p>
         </div>
+        <figure className="">
+          <img className="w-16" src={`/images/icons/react.svg`} alt="HTML" />
+        </figure>
       </header>
 
-      <figure className="">
-        <img
-          className="h-full inline-block"
-          src={`/images/icons/react-large.svg`}
-          alt="HTML"
-        />
-      </figure>
+      <div>
+        <ul className="list-disc pl-4 mt-8">
+          {module.topics.map((t, i) => {
+            return (
+              <li className="text-base text-gray-500 max-w-sm my-2" key={i}>
+                {t.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="flex items-center mt-8 text-gray-500 gap-x-2 border-t border-gray-200 pt-4">
+        <strong className="inline-block bg-gold-400 bg-opacity-40 p-2 rounded-md font-medium text-royal-blue-800">
+          Specific Module ★
+        </strong>
+        <span>•</span>
+        <strong className="text-gray-500 font-semibold">16-32 weeks</strong>
+      </div>
     </article>
   );
 }
