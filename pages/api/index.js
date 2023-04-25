3687d277f81e1b6723401d04ad5003239cb13ec4
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2023-04-25 22:53:54 +0530

    remove api

--- pages/api/index.js
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from 'next/server'
-
-export default function handler(req, res) {
-  console.log(req.geo, 'geolocation')
-  res.status(200).json({ name: 'John Doe', geo: req.geo })
-}
